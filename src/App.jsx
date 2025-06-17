ef49210055f344bc5d9235b2ef73d96e5baad4a6
unknown <tusharpaul7024@gmail.com>
2025-06-17 20:53:53 +0530

    created home page/landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Home from "./pages/Home";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router";
+
+const Router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/">
+      <Route index element={<Home />} />
+    </Route>
+  )
+);
 
 function App() {
-  return<></>;
-  
+  return <RouterProvider router={Router} />;
 }
+
 
 export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,32 @@
+import { useNavigate } from "react-router";
+
+
+const Home = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div className="container">
+      <div className="page">
+        <section className="content content-home">
+          <div className="text-container">
+            <h2 className="heading">Welcome to PopX</h2>
+            <p className="description">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </p>
+          </div>
+
+          <div className="button-container">
+            <button onClick={() => navigate("/create-account")}>
+              Create Account
+            </button>
+            <button onClick={() => navigate("/login")}>
+              Already Registered? Login
+            </button>
+          </div>
+        </section>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
